99f06377001a1795c720c6c159031ad85e3db0c8
zhengxiaodong <787182455@qq.com>
2022-10-17 11:19:59 +0800

    添加provider模块

---
+++ rentSystem-provider6666/src/main/java/com/zxd/rentsystemprovider6666/RentSystemProvider6666Application.java
@@ -0,0 +1,13 @@
+package com.zxd.rentsystemprovider6666;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentSystemProvider6666Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentSystemProvider6666Application.class, args);
+    }
+
+}
---
+++ rentSystem-provider6666/src/test/java/com/zxd/rentsystemprovider6666/RentSystemProvider6666ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxd.rentsystemprovider6666;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentSystemProvider6666ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
