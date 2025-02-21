f62bfadde83a5e4d91088b39d6b6be55e455238b
serverless-devs <serverless-devs@alibaba-inc.com>
2025-02-21 08:34:05 +0000

    "添加接口"

--- rentSystem-provider6666/src/main/java/com/zxd/rentsystem/controller/TestController.java
+++ rentSystem-provider6666/src/main/java/com/zxd/rentsystem/controller/TestController.java
@@ -1,11 +1,15 @@
 package com.zxd.rentsystem.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/test")
 @RestController
 public class TestController {
 
-    public void test(){
-
+    @GetMapping("/get")
+    public String get(){
+        return "hello world";
     }
 }
